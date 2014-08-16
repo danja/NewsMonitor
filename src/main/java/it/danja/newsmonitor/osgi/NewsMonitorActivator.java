df814d76e6d60e3372b939bcdc25125512e8fcaf
Danny Ayers <danny.ayers@gmail.com>
2014-08-16 12:44:35 +0200

    moving towards osgi (maven build is working, module not yet loadable)

---
+++ src/main/java/it/danja/newsmonitor/osgi/NewsMonitorActivator.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package it.danja.newsmonitor.osgi;
+
+import org.osgi.framework.BundleActivator;
+import org.osgi.framework.BundleContext;
+import org.osgi.framework.ServiceRegistration;
+import it.danja.newsmonitor.main.*;
+//import com.bw.osgi.provider.able.HelloWorldService;
+//import com.bw.osgi.provider.impl.HelloWorldServiceImpl;
+
+/**
+ *
+ * @author danny
+ */
+public class NewsMonitorActivator implements BundleActivator {
+    private ServiceRegistration registration;
+    private Main newsmonitor = new Main();
+
+    @Override
+    public void start(BundleContext bundleContext) throws Exception {
+    	
+        registration = bundleContext.registerService(
+                Main.class.getName(),
+                newsmonitor,
+                null);
+        newsmonitor.main(null); // TODO do it properly
+    }
+
+    @Override
+    public void stop(BundleContext bundleContext) throws Exception {
+    	
+        registration.unregister();
+        newsmonitor = null; // TODO do it properly
+    }
+}  
+
